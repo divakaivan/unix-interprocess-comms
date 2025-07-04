ee3ef18815846e0d65065b18b236154b378a3bbd
divakaivan <timee98@outlook.com>
2025-07-04 12:09:57 +0300

    ch.2 fork()

---
+++ forking.c
@@ -0,0 +1,34 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+    int rv;
+
+    // signal(SIGCHLD, SIG_IGN);
+    pid = fork();
+    switch (pid) {
+    case -1:
+        perror("fork");
+        exit(1);
+    case 0: // child
+        printf("this is a child process\n");
+        printf("pid = %d\n", getpid());
+        printf("pid's parent pid = %d\n", getppid());
+        printf("enter my exit status ");
+        scanf(" %d", &rv);
+        exit(rv);
+    default:
+        printf("this is a parent process\n");
+        printf("pid = %d\n", getpid());
+        printf("my child's pid is %d\n", pid);
+        printf("waiting for my child to exit()...\n");
+        wait(&rv);
+        printf("my child's exit status is: %d\n", WEXITSTATUS(rv));
+    }
+    return 0;
+}
